2326a14e06b38b57dc28ec9ecb130ec52521b5fc
Dustin <dfisher77@gmail.com>
2012-08-29 14:41:24 -0400

    finished section 3

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,7 +1,10 @@
-greeting = "Welcome!"
+player1 = "larry"
+player1_health = 60
 
-3.times do
-  puts greeting.upcase
-end
+player2 = "curly"
 
-puts Time.now
+player3 = "moe"
+
+puts "#{player1}'s health is #{player1_health}"
+
+puts "Players: \n\t#{player1}\n\t#{player2}\n\t#{player3}"
