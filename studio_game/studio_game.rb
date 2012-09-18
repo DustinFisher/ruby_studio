ae0bcd4ccc1b053a19ba7fb00cbc8ca249cdc88a
Dustin <dfisher77@gmail.com>
2012-09-18 11:28:28 -0400

    finished section 11

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,56 +1,4 @@
-class Player
-  attr_reader :health
-  attr_accessor :name
-
-  def initialize(name, health=100)
-    @name = name.capitalize
-    @health = health
-  end
-
-  def to_s
-    "I'm #{@name} with a health of #{@health} and a score of #{score}"
-  end
-
-  def blam
-    @health -= 10
-    puts "#{@name} got blammed!"
-  end
-
-  def w00t
-    @health += 15
-    puts "#{@name} go w00ted!"
-  end
-
-  def score
-    @health + @name.length
-  end
-end
-
-class Game
-  attr_reader :title
-
-  def initialize(title)
-    @title = title
-    @players = []
-  end
-
-  def add_player(player)
-    @players << player
-  end
-
-  def play
-    puts "There are #{@players.size} players in #{@title}}:"
-    @players.each do |player|
-      puts player
-    end
-    @players.each do |player|
-      player.blam
-      player.w00t
-      player.w00t
-      puts player
-    end
-  end
-end
+require_relative 'game'
 
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
@@ -61,14 +9,3 @@ knuckleheads.add_player(player1)
 knuckleheads.add_player(player2)
 knuckleheads.add_player(player3)
 knuckleheads.play
-
-player4 = Player.new("Alvin",135)
-player5 = Player.new("Simon",75)
-player6 = Player.new("Theodore",87)
-
-chipmunks = Game.new("Chipmunks")
-chipmunks.add_player(player4)
-chipmunks.add_player(player5)
-chipmunks.add_player(player6)
-chipmunks.add_player(player2)
-chipmunks.play
